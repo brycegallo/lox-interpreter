931383140df84ecc136cd6dcaafa3acf3b8ac0cb
bryce <76966016+brycegallo@users.noreply.github.com>
2024-09-10 19:57:22 -0600

    Add first functions create functions for interacting with lox interpreter

--- jlox/Lox.java
+++ jlox/Lox.java
@@ -19,4 +19,34 @@ public class Lox {
             runPrompt();
         }
     }
+
+    // runFile function, for when lox is run from the command line and given the path to a file to execute
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+    }
+
+    // runPrompt function, for when lox is run interactively
+    private static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        // reads line of input from user and returns result
+        for (;;) {
+            System.out.print("> ");
+            String line = reader.readLine();
+            if (line == null) break;
+            run(line);
+        }
+    }
+
+    private static void run(String source) {
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        // sort of place holder for just printing tokens for now
+        for (Token token : tokens) {
+            System.out.println(token);
+        }
+    }
 }
