322c96fb5ae22470c80fc73deba6fabbd48fadc5
bryce <76966016+brycegallo@users.noreply.github.com>
2024-09-10 21:00:43 -0600

    Modify Scanner.java add hashmap for keywords

--- jlox/Scanner.java
+++ jlox/Scanner.java
@@ -8,6 +8,27 @@ import java.util.Map;
 import static com.craftinginterpreters.lox.TokenType.*;
 
 class Scanner {
+    private static final Map<String, TokenType> keywords;
+
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and",    AND);
+        keywords.put("class",  CLASS);
+        keywords.put("else",   ELSE);
+        keywords.put("false",  FALSE);
+        keywords.put("for",    FOR);
+        keywords.put("fun",    FUN);
+        keywords.put("if",     IF);
+        keywords.put("nil",    NIL);
+        keywords.put("or",     OR);
+        keywords.put("print",  PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super",  SUPER);
+        keywords.put("this",   THIS);
+        keywords.put("true",   TRUE);
+        keywords.put("var",    VAR);
+        keywords.put("while",  WHILE);
+    }
     private final String source;
     private final List<Token> tokens = new ArrayList<>();
     // int start and int current are offsets that index into the string
